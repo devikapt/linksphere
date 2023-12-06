4bf79f4190d3c5e1762a3ac124bf1294ce543926
Devika <devikalagesh20@gmail.com>
2023-12-06 09:27:24 -0800

    social

--- social/views.py
+++ social/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import FormView,CreateView,TemplateView,View
+from social.forms import RegistrationForm,LoginForm
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
+
+class SignUpView(CreateView):
+    template_name="register.html"
+    form_class=RegistrationForm
+
+    def get_success_url(self):
+        return reverse("signin")
+    
+class SignInView(FormView):
+    template_name="login.html"
+    form_class=LoginForm
+
+    def post(self,request,*args,**kwargs):
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            uname=form.cleaned_data.get("username")
+            pwd=form.cleaned_data.get("password")
+            user_object=authenticate(request,username=uname,password=pwd)
+            if user_object:
+                login(request,user_object)
+                print("login is successfully.......")
+                return redirect("index")
+
+            print("error in login")
+            return redirect(request,"login.html",{"form":form})
+        
+class IndexView(TemplateView):
+    template_name="index.html"
+
+class SignOutView(View):
+    def get(self,request,*args,**kwargs):
+        logout(request)
+        return redirect("signin")
+
+
+
+
+
+    # def post(self,request,*args,**kwargs):
+    #     form=RegistrationForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect("register")
+    #     else:
+    #         return render(request,"register.html",{form:form})
